1c7dbab90acd01997e954f2de045300c26cc6f2f
chrisbruck <c.bruck@gmx.de>
2014-12-26 21:09:59 +0100

    relativer pfad notwendig

--- migrationstreams/src/index.php
+++ migrationstreams/src/index.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/controller/CountryController.php';
 require_once __DIR__.'/controller/DenominationController.php';
 require_once __DIR__.'/controller/ProfessionalCategoryController.php';
 require_once __DIR__.'/../../vendor/propel/runtime/lib/Propel.php';
-Propel::init("/conf/migrationstreams-conf.php");
+Propel::init(__DIR__."/conf/migrationstreams-conf.php");
 set_include_path("/model" . PATH_SEPARATOR . get_include_path());
 
 $app = new Silex\Application();
