8453511b530cdd62ac1dc49af32382671626df9f
schickerel <schicker.elen@gmail.com>
2015-02-25 19:20:50 +0100

    show migrations of a person in console.log

--- migrationstreams/web/js/person.js
+++ migrationstreams/web/js/person.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    var map = new Datamap({
+        element: document.getElementById('map-wrapper'),
+        fills: {
+            defaultFill: "#000000"
+        }
+    });
+
     $('#filter').click(function(){
         var denominationIds = "";
         var professionalCategoryIds = "";
@@ -47,9 +54,18 @@ $(document).ready(function(){
         $.getJSON("../src/index.php/person/persons" + url)
             .done(function(json) {
                 $.each(json, function(index, person){
-                    $('#person-list').append("<li>" + person['FirstName'] + " " + person['LastName'] + "</li>");
+                    $('#person-list').append("<li value='" + person['Id'] + "'>" + person['FirstName'] + " " + person['LastName'] + "</li>");
                 });
             });
     };
 
+    $('#person-list').on('click', 'li', function(){
+        var personId = $(this).val();
+        $.getJSON("../src/index.php/migration/migrations?personId=" + personId)
+            .done(function(json) {
+                $.each(json, function(index, migration){
+                    console.log(migration);
+                });
+            });
+    });
 });
